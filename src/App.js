a0288037600cab729c8efd99e487c2475dade3cc
OEM Configuration (temporary user) <arkadiuszmartyniuk@interia.pl>
2022-08-17 21:20:36 +0200

    slider added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Slider from 'react-smooth-range-input';
 
 export function App() {
+
+    const [pigtails, setPigtails] = useState('');
+
     return (
         <div>
             <h1>Welcome! the date is: {new Date().toDateString()}</h1>
+            <Slider value={0} min={0} max={30} onChange={(x) => setPigtails(x)} />
+            <h2>{pigtails}</h2>
         </div>
     )
 }
